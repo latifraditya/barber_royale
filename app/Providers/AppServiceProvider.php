83971065043756c17ec9eb983da627ce34fc5ff0
latifraditya <latif.raditya@sci.ui.ac.id>
2025-03-25 08:13:45 +0700

    Add auth roles admin and reguler user

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+
+        Gate::define('admin', function($user){
+            return $user->role == 'admin';
+        });
     }
 }
