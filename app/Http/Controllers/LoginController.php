7c3b421de5149922cf5ac4cedcb1bd52713185be
latifraditya <latif.raditya@sci.ui.ac.id>
2025-04-11 14:40:09 +0700

    Add home and login controller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -24,7 +24,7 @@ class LoginController extends Controller
 
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
-            return redirect()->intended('bookings');
+            return redirect()->intended('/');
         }
 
         return back()->with('loginError', 'Login failed!');
