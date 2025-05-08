76d4b7830d24196cceaf6fd12ca13bae994bbd8b
latifraditya <latif.raditya@sci.ui.ac.id>
2025-05-08 14:24:05 +0700

    Add transaction and detail transaction table

---
+++ database/migrations/2025_04_15_020459_create_transactions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('booking_id')->constrained()->onDelete('cascade');
+            $table->decimal('total_before_tax', 10, 2);
+            $table->decimal('tax_percent', 5, 2)->default(0.1);
+            $table->decimal('total_after_tax', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
