f85add62dd41aaec72728586d4eee3a6f9d60293
latifraditya <latif.raditya@sci.ui.ac.id>
2025-05-21 10:15:29 +0700

    Add sidebar for dashboard (users, transactions, sales)

--- resources/views/admin/dashboard/layouts/sidebar.blade.php
+++ resources/views/admin/dashboard/layouts/sidebar.blade.php
@@ -8,63 +8,31 @@
         </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link {{ Request::is('dashboard/posts*') ? 'active' : " " }}" href="/dashboard/posts">
+        <a class="nav-link {{ Request::is('dashboard/users*') ? 'active' : " " }}" href="/dashboard">
           <i class="bi bi-calendar-check me-2"></i>
           User Bookings
         </a>
       </li>
 
       <li class="nav-item">
+        <a class="nav-link {{ Request::is('dashboard/transaction*') ? 'active' : '' }}" aria-current="page" href="/dashboard/transactions">
+          <i class="bi bi-receipt-cutoff me-2"></i> Transaksi
+        </a>
+      </li>
+
+      <li class="nav-item">
         <a class="nav-link {{ Request::is('dashboard/sales*') ? 'active' : '' }}" aria-current="page" href="/dashboard/sales">
-          <i class="bi bi-clock-history me-2"></i>
-          History
-        </a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link {{ Request::is('dashboard/supermarkets*') ? 'active' : '' }}" aria-current="page" href="/dashboard/supermarkets">
-          <i class="bi bi-receipt-cutoff me-2"></i>   Transaksi
-        </a>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link {{ Request::is('dashboard/supermarkets*') ? 'active' : '' }}" aria-current="page" href="/dashboard/supermarkets">
           <i class="bi bi-bar-chart-line me-2"></i>
           Sales & Revenue
         </a>
       </li>
 
       <li class="nav-item">
-        <a class="nav-link {{ Request::is('dashboard/supermarkets*') ? 'active' : '' }}" aria-current="page" href="/dashboard/supermarkets">
+        <a class="nav-link {{ Request::is('dashboard/settings*') ? 'active' : '' }}" aria-current="page" href="/dashboard/settings">
           <i class="bi bi-gear me-2"></i> 
           Settings
         </a>
       </li>
     </ul>
-
-    @can('admin')
-    <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-      <span>Administrator</span>
-    </h6>
-    
-    <ul class="nav flex-column">
-      <li class="nav-item">
-        <a class="nav-link {{ Request::is('dashboard/categories*') ? 'active' : '' }}"href="/dashboard/categories">
-          <span data-feather="grid"></span>
-          Post Categories
-        </a>
-      </li>
-    </ul>
-
-    <ul class="nav flex-column">
-      <li class="nav-item">
-        <a class="nav-link {{ Request::is('dashboard/users*') ? 'active' : '' }}"href="/dashboard/users">
-          <span data-feather="users"></span>
-          Manage User
-        </a>
-      </li>
-    </ul>
-    @endcan
-
   </div>
 </nav>
